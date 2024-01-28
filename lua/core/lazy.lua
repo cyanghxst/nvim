0df0d067f5f1fc84c8c00133aafc5019a56fcea1
razor6ack <155308268+razor6ack@users.noreply.github.com>
2024-01-29 00:05:53 +0700

    reformatted and added ui border to lazy.lua

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -1,20 +1,28 @@
 -- Automatically install Lazy on startup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Automatically source all configs in plugins directory
 require("lazy").setup("plugins", {
-    change_detection = {
-        notify = false,
-    },
+	change_detection = {
+		notify = false,
+	},
+	-- ui = { |nvim_open_win()| },
+	ui = {
+		border = "single",
+		size = {
+			width = 0.8,
+			height = 0.8,
+		},
+	},
 })

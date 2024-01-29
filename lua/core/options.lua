5f6f2062c9cd6c068d978e52b6c2d70131353b93
razor6ack <155308268+razor6ack@users.noreply.github.com>
2024-01-29 15:59:53 +0700

    enabled relative line number in options.lua

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -40,6 +40,11 @@ opt.clipboard = "unnamedplus"
 -- Cursor
 cmd[[set guicursor+=i:blinkon1]] -- Enable cursor blinking
 
+-- Relative line numbers
+vim.api.nvim_set_option('number', false)
+vim.api.nvim_set_option('relativenumber', true)
+
 -- Highlight groups
 -- vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", { fg = "#3b4261" })
 -- vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "#3b4261" })
+

e171bcfe8dc1bde0368aa0b49a947153802eac02
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-01-30 17:40:18 +0700

    changed config in alpha.lua

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,6 +1,6 @@
 return {
     "goolord/alpha-nvim",
-    -- enabled = true,
+    enabled = true,
     event = "VimEnter",
     opts = function()
         local dashboard = require("alpha.themes.dashboard")
@@ -13,20 +13,17 @@ return {
    █████████ ██████████ █████████ █████ █████ ████ █████  
  ███████████ ███    ███ █████████ █████ █████ ████ █████ 
 ██████  █████████████████████ ████ █████ █████ ████ ██████
-
-
         ]]
 
         dashboard.section.header.val = vim.split(logo, "\n")
         dashboard.section.buttons.val = {
-            dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-            dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-            dashboard.button("g", "󰷾 " .. " Find text", ":Telescope live_grep <CR>"),
-            dashboard.button("b", " " .. " File browser", ":Joshuto <CR>"),
-            dashboard.button("r", "󰄉 " .. " Recent files", ":Telescope oldfiles <CR>"),
-            dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-            dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
-            dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+            dashboard.button("f", " " .. " Find File", ":Telescope find_files<CR>"),
+            dashboard.button("t", " " .. " Find Text", ":Telescope live_grep<CR>"),
+            dashboard.button("n", " " .. " New File", ":ene | startinsert<CR>"),
+            dashboard.button("r", " " .. " Recent", ":Telescope oldfiles<CR>"),
+            dashboard.button("u", " " .. " Update", ":Lazy update<CR>"),
+            dashboard.button("s", " " .. " Settings", ":cd ~/.config/nvim | :e lua/plugins/alpha.lua | :NvimTreeToggle<CR>"),
+            dashboard.button("q", " " .. " Quit", ":qa!<CR>"),
         }
 
         -- set highlight
@@ -34,10 +31,11 @@ return {
             button.opts.hl = "AlphaButtons"
             button.opts.hl_shortcut = "AlphaShortcut"
         end
+
         dashboard.section.header.opts.hl = "AlphaHeader"
         dashboard.section.buttons.opts.hl = "AlphaButtons"
         dashboard.section.footer.opts.hl = "AlphaFooter"
-        dashboard.opts.layout[1].val = 8
+        dashboard.opts.layout[1].val = 5
         return dashboard
     end,
     config = function(_, dashboard)
@@ -68,122 +66,3 @@ return {
         })
     end,
 }
-
--- return {
---     "goolord/alpha-nvim",
---     enabled = false,
---     config = function()
-
---         local present, alpha = pcall(require, "alpha")
---         if not present then
---             return "Alpha has an error"
---         end
-
---         local header = {
---             type = "text",
---             val = {
-
---                 [[                                                                   ]],
---                 [[      ████ ██████           █████      ██                    ]],
---                 [[     ███████████             █████                            ]],
---                 [[     █████████ ███████████████████ ███   ███████████  ]],
---                 [[    █████████  ███    █████████████ █████ ██████████████  ]],
---                 [[   █████████ ██████████ █████████ █████ █████ ████ █████  ]],
---                 [[ ███████████ ███    ███ █████████ █████ █████ ████ █████ ]],
---                 [[██████  █████████████████████ ████ █████ █████ ████ ██████]],
-
---             },
---             opts = {
---                 position = "center",
---                 hl = "Function",
---             },
---         }
-
---         local function button(sc, txt, keybind)
---             local sc_ = sc:gsub("%s", ""):gsub("SPC", "<leader>")
---             local opts = {
---                 position = "center",
---                 text = txt,
---                 shortcut = sc,
---                 cursor = 4,
---                 width = 19,
---                 align_shortcut = "right",
---                 hl_shortcut = "Number",
---                 hl = "Function",
---             }
---             if keybind then
---                 opts.keymap = { "n", sc_, keybind, { noremap = true, silent = true } }
---             end
---             return {
---                 type = "button",
---                 val = txt,
---                 on_press = function()
---                     local key = vim.api.nvim_replace_termcodes(sc_, true, false, true)
---                     vim.api.nvim_feedkeys(key, "normal", false)
---                 end,
---                 opts = opts,
---             }
---         end
-
---         local buttons = {
---             type = "group",
---             val = {
---                 button("f", "   Find Files                          ", ":Telescope find_files<CR>"),
---                 button("t", "   Find Text                           ", ":Telescope live_grep<CR>"),
---                 button("n", "   New Files                           ", ":ene <BAR> startinsert<CR>"),
---                 button("r", "   Recents                             ", ":Telescope oldfiles<CR>"),
---                 button("u", "   Update                              ", ":Lazy update<CR>"),
---                 button("s", "   Settings                            " ,":cd ~/.config/nvim | :e lua/plugins/alpha.lua | :NvimTreeToggle<CR>"),
---                 button("q", "   Quit                                ", ":qa!<CR>"),
---             },
---             opts = {
---                 position = "center",
---                 spacing = 1,
---             },
---         }
-
---         local function footer()
---             local stats = require("lazy").stats()
---             local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
---             return  "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms"
---         end
-
---         -- local function footer()
---         --     local plugins = #vim.tbl_keys(require("lazy").plugins())
---         --     local version = vim.version()
---         --     local nvim_version_info = version.major .. "." .. version.minor .. "." .. version.patch
---         --     return "󰋼  " .. nvim_version_info .. "    " .. "  " .. plugins .. " Plugins"
---         -- end
-
---         local footer = {
---             type = "text",
---             val = footer,
---             opts = {
---                 position = "center",
---                 hl = "DiagnosticUnnecessary",
---             },
---         }
-
---         local section = {
---             header = header,
---             buttons = buttons,
---             footer = footer,
---         }
-
---         local opts = {
---             layout = {
---                 { type = "padding", val = 5 },
---                 section.header,
---                 { type = "padding", val = 3 },
---                 section.buttons,
---                 { type = "padding", val = 2 },
---                 section.footer,
---             },
---             opts = {
---                 margin = 44,
---             },
---         }
-
---         alpha.setup(opts)
---     end,
--- }

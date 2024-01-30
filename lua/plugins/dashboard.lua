98a832ce5a8286df4421be3f4d451e2ce1e95165
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-01-30 16:15:23 +0700

    changed button format in dashboard.lua

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -28,13 +28,13 @@ return {
                 header = vim.split(logo, "\n"),
                 -- stylua: ignore
                 center = {
-                    { action = "Telescope find_files",                                               desc = " Find Files", icon = "  ", key = "f" },
-                    { action = "Telescope live_grep",                                                desc = " Find Test",  icon = "  ", key = "t" },
-                    { action = "ene | startinsert",                                                  desc = " New Files",  icon = "  ", key = "n" },
-                    { action = "Telescope oldfiles",                                                 desc = " Recents",    icon = "  ", key = "r" },
-                    { action = "Lazy update",                                                        desc = " Update",     icon = "  ", key = "u" },
-                    { action = "cd ~/.config/nvim | :e lua/plugins/alpha.lua | :NvimTreeToggle<CR>", desc = " Settings",   icon = "  ", key = "s" },
-                    { action = "qa!",                                                                desc = " Quit",       icon = "  ", key = "q" },
+                    { action = "Telescope find_files",                                               desc = " Find Files", icon = " ", key = "f" },
+                    { action = "Telescope live_grep",                                                desc = " Find Test",  icon = " ", key = "t" },
+                    { action = "ene | startinsert",                                                  desc = " New Files",  icon = " ", key = "n" },
+                    { action = "Telescope oldfiles",                                                 desc = " Recents",    icon = " ", key = "r" },
+                    { action = "Lazy update",                                                        desc = " Update",     icon = " ", key = "u" },
+                    { action = "cd ~/.config/nvim | :e lua/plugins/alpha.lua | :NvimTreeToggle<CR>", desc = " Settings",   icon = " ", key = "s" },
+                    { action = "qa!",                                                                desc = " Quit",       icon = " ", key = "q" },
                 },
 
                 -- footer = function()
@@ -54,7 +54,7 @@ return {
 
         for _, button in ipairs(opts.config.center) do
             button.desc = button.desc .. string.rep(" ", 33 - #button.desc)
-            button.key_format = "%s"
+            button.key_format = " %s"
         end
 
         -- close Lazy and re-open when the dashboard is ready

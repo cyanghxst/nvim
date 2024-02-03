ae9a96f6618f4abdff66431bb873dfc733ded342
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-02-03 19:49:30 +0700

    changed padding option in alpha.lua

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -44,8 +44,8 @@ return {
         dashboard.section.footer.opts.hl = "AlphaFooter"
 
         -- Set padding
-        dashboard.opts.layout[1].val = 3
-        -- dashboard.opts.layout[1].val = 6
+        -- dashboard.opts.layout[1].val = 3
+        dashboard.opts.layout[1].val = 6
         dashboard.opts.layout[3].val = 3
         table.insert(dashboard.config.layout, 5, {
             type = 'padding',

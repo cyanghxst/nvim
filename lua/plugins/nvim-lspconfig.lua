b628a4a9fd357d7e85e13111a8ddfb7b0b584402
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-05-15 10:01:11 -0700

    added lsp setup and keybinds in nvim-lspconfig.lua

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -3,5 +3,10 @@ return {
     config = function()
         local lspconfig = require("lspconfig")
         lspconfig.lua_ls.setup({})
+        lspconfig.tsserver.setup({})
+        lspconfig.html.setup({})
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+        vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})
     end
 }

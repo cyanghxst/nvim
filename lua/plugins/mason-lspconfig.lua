c14ed17831d6cff87051b4eae688303b81172692
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-05-15 09:55:35 -0700

    modified mason-lsp.config.lua

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -2,7 +2,11 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
         require("mason-lspconfig").setup({
-            ensure_installed = { "lua_ls" }
+            ensure_installed = {
+                "lua_ls",
+                "tsserver",
+                "html",
+            }
         })
     end
 }

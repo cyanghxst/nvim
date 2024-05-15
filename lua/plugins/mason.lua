ef4c50e590a74532783aa9500409081fc20e5789
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-05-15 02:38:50 -0700

    modified mason.lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,36 +1,6 @@
 return {
     "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-    },
     config = function()
-        -- import mason
-        local mason = require("mason")
-
-        -- import mason lsp config
-        local mason_lspconfig = require("mason-lspconfig")
-
-        mason.setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
-                },
-            },
-        })
-
-        mason_lspconfig.setup({
-            -- list of servers for mason to install
-            ensure_installed = {
-                "tsserver",
-                "html",
-                "cssls",
-                "pyright",
-            },
-
-            -- auto-install configured servers (with lspconfig)
-            automatic_installation = true,
-        })
-    end,
+        require("mason").setup()
+    end
 }

afa33f4ad54863de1a993aa7db259dde4caed594
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-05-15 02:46:09 -0700

    added disable lsp semantic tokens to autocmds.lua

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -27,3 +27,11 @@ augroup END
 vim.cmd([[
   au BufReadPost * if expand('%:p') !~# '\m/\.git/' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
 ]])
+
+-- Disable lsp semantic tokens
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
+});

81ceb96241482a1b39fec2ce43bed5049638832f
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-05-17 20:33:05 -0700

    modified treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,14 +1,44 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    enabled = true,
-    dependencies = {
-        "windwp/nvim-ts-autotag",
-    },
-    config = function()
-        require'nvim-treesitter.configs'.setup {
-            autotag = {
-                enable = true,
-            }
+    opts = {
+        auto_install = true,
+        autotag = {
+            enable = true,
+            filetypes = {
+                "html", "javascript", "typescript", "svelte", "vue", "tsx", "jsx", "rescript",
+                "css", "lua", "xml", "php", "markdown"
+            },
+        },
+        indent = { enable = true },
+        ensure_installed = {
+            -- defaults 
+            "vim",
+            "lua",
+            "vimdoc",
+
+            -- web dev 
+            "html",
+            "css",
+            "javascript",
+            "typescript",
+            "tsx",
+            "astro",
+            "vue",
+            "svelte",
+            "markdown",
+            "markdown_inline",
+            "json",
+            "scss",
+            "yaml"
         }
-    end
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        config = function ()
+            require('nvim-ts-autotag').setup()
+        end,
+        lazy = true,
+        event = "VeryLazy"
+    },
 }

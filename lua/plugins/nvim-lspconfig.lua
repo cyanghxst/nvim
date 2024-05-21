066f45f78f402cb75e13df4ca2846257cda096bc
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-05-21 16:02:14 -0700

    modify nvim-lspconfig.lua

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -12,6 +12,7 @@ return {
         lspconfig.cssls.setup({})
         lspconfig.tsserver.setup({})
         lspconfig.html.setup({})
+        lspconfig.tailwindcss.setup({})
         vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
         vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})

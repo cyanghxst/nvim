5daf717de8c1e21ee43da225ccd4fc90927c70dd
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-05-25 16:39:48 -0700

    modify virtual_text in nvim-lspconfig.lua

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -14,9 +14,9 @@ return {
         lspconfig.tsserver.setup({})
         lspconfig.html.setup({})
         lspconfig.tailwindcss.setup({})
-        vim.diagnostic.config({
-            virtual_text = false,
-        })
+        -- vim.diagnostic.config({
+        --     virtual_text = false,
+        -- })
         vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
         vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})

7fe74cc0d4621a28da784a6ec457c25ee158fb5c
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-05-26 10:46:53 -0700

    modify padding in alpha.lua

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -47,7 +47,7 @@ return {
         -- Set padding
         -- dashboard.opts.layout[1].val = 3
         -- dashboard.opts.layout[1].val = 6
-        dashboard.opts.layout[1].val = 3
+        dashboard.opts.layout[1].val = 2
         dashboard.opts.layout[3].val = 4
         table.insert(dashboard.config.layout, 5, {
             type = 'padding',

e6c8cc6c1582b6ca7dde65ac4ce557e654a64598
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-05-25 17:09:13 -0700

    add nvim-ts-autotag.lua

---
+++ lua/plugins/nvim-ts-autotag.lua
@@ -0,0 +1,11 @@
+return {
+    "windwp/nvim-ts-autotag",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    lazy = true,
+    event = "VeryLazy",
+    config = function ()
+        require('nvim-ts-autotag').setup({
+            -- your config
+        })
+    end,
+}

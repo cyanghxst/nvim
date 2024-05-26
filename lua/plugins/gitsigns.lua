9ec2702690262befe53fbd6d3fa3e9692fa5d868
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-05-26 15:13:09 -0700

    modify gitsigns.lua

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -4,19 +4,20 @@ return {
     enabled = true,
     config = {
         numhl = false,
+        -- signcolumn = false,
         linehl = false,
         watch_gitdir = {
             interval = 1000,
             follow_files = true,
         },
-        -- signs = {
-        --     add = { hl = "GitSignsAdd", text = "⎮", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-        --     change = { hl = "GitSignsChange", text = "⎮", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-        --     delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        --     topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        --     changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-        --     untracked = { hl = "GitSignsAdd", text = "┆", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-        -- },
+        signs = {
+            add = { hl = "GitSignsAdd", text = "⎮", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+            change = { hl = "GitSignsChange", text = "⎮", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+            delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+            topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+            changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+            untracked = { hl = "GitSignsAdd", text = "┆", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+        },
         current_line_blame = false,
         sign_priority = 6,
         update_debounce = 100,

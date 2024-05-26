3ebc362c0ddace37f0edf28c46597d3b1d1c622a
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-05-25 22:13:26 -0700

    modify nvim-treesitter.lua

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,3 +1,11 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = "windwp/nvim-ts-autotag"
+    opts = {
+        ensure_installed = 'all',
+        auto_install = true,
+        highlight = { enable = true, },
+        indent = { enable = true, },
+        autotag = { enable = false }, -- this breaks dot repeating with `>`
+    }
 }

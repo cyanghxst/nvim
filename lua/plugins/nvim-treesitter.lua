7736a7143e648ea2101dffb2a44c9a7f17a94384
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-05-25 17:08:09 -0700

    modify nvim-treesitter.lua

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,13 +1,3 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-        "windwp/nvim-ts-autotag",
-    },
-    config = function()
-        require"nvim-treesitter.configs".setup {
-            autotag = {
-                enable = true,
-            }
-        }
-    end
 }

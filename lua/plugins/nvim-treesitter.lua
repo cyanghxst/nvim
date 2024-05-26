dbe782268a15c07adce2f69990b5651f598c0403
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-05-25 22:28:53 -0700

    modify nvim-treesitter.lua

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,8 +1,27 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = "windwp/nvim-ts-autotag"
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    build = ":TSUpdate",
+    -- dependencies = "windwp/nvim-ts-autotag",
     opts = {
-        ensure_installed = 'all',
+        ensure_installed = {
+            -- defaults 
+            "vim",
+            "lua",
+            "vimdoc",
+
+            -- web dev 
+            "html",
+            "css",
+            "javascript",
+            "typescript",
+            "tsx",
+
+            -- low level
+            "c",
+            "zig"
+        },
         auto_install = true,
         highlight = { enable = true, },
         indent = { enable = true, },

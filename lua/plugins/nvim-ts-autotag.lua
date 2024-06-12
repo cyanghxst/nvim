11d443ce5145de9e906560f5f28b9e48cd583cee
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-06-11 22:13:35 -0700

    modify nvim-ts-autotag.lua

--- lua/plugins/nvim-ts-autotag.lua
+++ lua/plugins/nvim-ts-autotag.lua
@@ -1,7 +1,7 @@
 return {
     "PriceHiller/nvim-ts-autotag",
     enabled = true,
-    dependencies = "nvim-treesitter/nvim-treesitter",
+    dependencies = { "nvim-treesitter/nvim-treesitter", opts = { auto_install = true } },
     event = { "BufReadPre", "BufNewFile" },
     config = function()
         require("nvim-ts-autotag").setup({

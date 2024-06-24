afa3975964f887e9bdfadfd6414532c10320d4f8
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-06-23 23:18:22 -0700

    modify nvim-lspconfig.lua

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,6 @@
 return {
     "neovim/nvim-lspconfig",
-    enabled = true,
+    enabled = false,
     event = { "BufReadPost", "BufNewFile" },
     dependencies = "hrsh7th/cmp-nvim-lsp",
     config = function()

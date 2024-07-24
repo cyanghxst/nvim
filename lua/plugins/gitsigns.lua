b7dfb7ffb78df809413ac884ab0fb6fc43c283e3
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-07-24 01:49:00 -0700

    modify gitsigns.lua: updated and removed deprecated options

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -10,14 +10,25 @@ return {
             interval = 1000,
             follow_files = true,
         },
+
         signs = {
-            add = { hl = "GitSignsAdd", text = "⎮", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-            change = { hl = "GitSignsChange", text = "⎮", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-            delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-            topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-            changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-            untracked = { hl = "GitSignsAdd", text = "┆", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+            add          = { text = "⎮" },
+            change       = { text = "⎮" },
+            delete       = { text = "_" },
+            topdelete    = { text = "‾" },
+            changedelete = { text = "~" },
+            untracked    = { text = "┆" },
         },
+
+        signs_staged = {
+            add          = { text = "⎮" },
+            change       = { text = "⎮" },
+            delete       = { text = "_" },
+            topdelete    = { text = "‾" },
+            changedelete = { text = "~" },
+            untracked    = { text = "┆" },
+        },
+
         current_line_blame = false,
         sign_priority = 6,
         update_debounce = 100,

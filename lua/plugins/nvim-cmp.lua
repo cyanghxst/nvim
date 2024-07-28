a355e2b1089c21252a7c8c30fd46dbdbfdac96bc
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-07-28 16:18:41 -0700

    modify nvim-cmp.lua: added icons

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -40,7 +40,35 @@ return {
     config = function()
         local cmp = require("cmp")
         local luasnip = require("luasnip")
-        local lspkind = require('lspkind')
+        -- local lspkind = require('lspkind')
+        local icons = {
+            Text = "󰉿",
+            Method = "󰆧",
+            Function = "󰊕",
+            Constructor = "",
+            Field = "󰜢",
+            Variable = "󰀫",
+            Class = "󰠱",
+            Interface = "",
+            Module = "",
+            Property = "󰜢",
+            Unit = "󰑭",
+            Value = "󰎠",
+            Enum = "",
+            Keyword = "󰌋",
+            Snippet = "",
+            Color = "󰏘",
+            File = "󰈙",
+            Reference = "󰈇",
+            Folder = "󰉋",
+            EnumMember = "",
+            Constant = "󰏿",
+            Struct = "󰙅",
+            Event = "",
+            Operator = "󰆕",
+            TypeParameter = "",
+        }
+
         luasnip.config.setup({})
 
         cmp.setup({
@@ -91,22 +119,13 @@ return {
                 { name = "path" },
             },
 
-            -- formatting = {
-            --     format = lspkind.cmp_format({
-            --         mode = 'symbol', -- show only symbol annotations
-            --         maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-            --         -- can also be a function to dynamically calculate max width such as 
-            --         -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-            --         ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-            --         show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+            formatting = {
+                format = function(_, vim_item)
+                    vim_item.kind = (icons[vim_item.kind] or "Foo") .. "  " .. vim_item.kind
+                    return vim_item
+                end,
+            }
 
-            --         -- The function below will be called before any actual modifications from lspkind
-            --         -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-            --         before = function (entry, vim_item)
-            --             return vim_item
-            --         end
-            --     })
-            -- }
         })
     end,
 }

516ac771dc9069d23b0e239a252035839e1b56c3
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-07-28 18:56:42 -0700

    modify nvim-web-devicons: overrided js icon

--- lua/plugins/nvim-web-devicons.lua
+++ lua/plugins/nvim-web-devicons.lua
@@ -1,4 +1,14 @@
 return {
     "nvim-tree/nvim-web-devicons",
     enabled = true,
+    config = {
+        override = {
+            js = {
+                icon = "",
+                color = "#F7E019",
+                -- cterm_color = "65",
+                name = "Javascript"
+            }
+        };
+    }
 }

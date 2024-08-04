c5c76bd88d843e86937aacee24d437d8c2c07f09
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-08-03 23:07:39 -0700

    feat(nvim-treesitter): update disabled languages for highlight

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -8,7 +8,7 @@ return {
         highlight = {
             enable = true,
             additional_vim_regex_highlighting = false,
-            -- disable = { "html" }
+            disable = { "tmux" }
         },
 
         indent = {

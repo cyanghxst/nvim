41a7fa97955692efe310a37e3c1ca3c410dcc043
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-08-19 04:42:27 -0700

    feat(nvim-lspconfig): add jdtls setup

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -9,6 +9,10 @@ return {
         -- local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+        lsp.jdtls.setup({
+            capabilities = capabilities,
+        })
+
         lsp.lua_ls.setup({
             capabilities = capabilities,
             settings = {

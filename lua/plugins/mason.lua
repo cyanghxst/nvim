5da6699a642c03b808ededd19f4d39f2c798d001
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-08-19 20:32:44 -0700

    feat(mason): add `mason-tool-installer` as a dependency and setup

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,7 +1,8 @@
 return {
     "williamboman/mason.nvim",
     dependencies = {
-        "williamboman/mason-lspconfig.nvim",
+        { "williamboman/mason-lspconfig.nvim" },
+        { "WhoIsSethDaniel/mason-tool-installer.nvim" },
     },
     config = function()
         require("mason").setup({
@@ -22,6 +23,30 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = { "lua_ls", "pyright", "tsserver", "jdtls" }, -- List of servers to automatically install
             automatic_installation = true, -- Automatically install servers
+        })
+
+        require("mason-tool-installer").setup({
+            ensure_installed = {
+                "java-debug-adapter",
+                "java-test",
+            }
+        })
+
+        local lspconfig = require('lspconfig')
+        local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+        local lsp_attach = function(client, bufnr)
+            -- Create your keybindings here... 
+        end
+
+        require('mason-lspconfig').setup_handlers({
+            function(server_name)
+                if server_name ~= 'jdtls' then
+                    lspconfig[server_name].setup({
+                        on_attach = lsp_attach,
+                        capabilities = lsp_capabilities,
+                    })
+                end
+            end
         })
     end
 }
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -9,9 +9,9 @@ return {
         -- local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-        lsp.jdtls.setup({
-            capabilities = capabilities,
-        })
+        -- lsp.jdtls.setup({
+        --     capabilities = capabilities,
+        -- })
 
         lsp.lua_ls.setup({
             capabilities = capabilities,

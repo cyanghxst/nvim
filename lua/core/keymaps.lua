1a3d9744b90bf556b1626cf0f415cf7d8f01cce5
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-08-25 02:34:45 -0700

    refactor(keymaps): reconfigure keymaps with `vim.keymap.set`

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,39 +1,39 @@
-local map = vim.api.nvim_set_keymap
+local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- Lazy
-map("n", "<leader>l", "<cmd>Lazy<cr>", opts)
+keymap("n", "<leader>l", "<cmd>Lazy<cr>", opts)
 
 -- Toggle highlight off by hitting carriage return
-map("n", "<cr>", "<cmd>nohlsearch<cr>", opts)
+keymap("n", "<cr>", "<cmd>nohlsearch<cr>", opts)
 
 -- Toggle highlight off by hitting escape key
-map("n", "<esc>", "<cmd>nohlsearch<cr>", opts)
+keymap("n", "<esc>", "<cmd>nohlsearch<cr>", opts)
 
 -- Better up/down
-map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, noremap = true, silent = true })
-map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, noremap = true, silent = true })
+keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
 -- Better window navigation
-map("n", "<C-h>", "<C-w>h", opts)
-map("n", "<C-j>", "<C-w>j", opts)
-map("n", "<C-k>", "<C-w>k", opts)
-map("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Buffers
-map("n", "H", "<cmd>bprevious<cr>", opts)
-map("n", "L", "<cmd>bnext<cr>", opts)
+keymap("n", "H", "<cmd>bprevious<cr>", opts)
+keymap("n", "L", "<cmd>bnext<cr>", opts)
 
 -- Move blocks in visual mode
-map("v", "J", ":m '>+1<CR>gv=gv", opts)
-map("v", "K", ":m '<-2<CR>gv=gv", opts)
+keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
 
 -- Indent blocks in visual mode
-map("v", "<", "<gv", opts)
-map("v", ">", ">gv", opts)
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
 
 -- Disable floating window when the return key is hit
-vim.keymap.set("n", "<cr>", function()
+keymap("n", "<cr>", function()
     local found_float = false
     for _, win in ipairs(vim.api.nvim_list_wins()) do
         if vim.api.nvim_win_get_config(win).relative ~= '' then

20037d745d3b3fa6e03d2bdb339cacaf4c9da59a
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-08-25 01:24:02 -0700

    feat(nvim-dap): add nvim-dap-ui as its dependency

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,5 +1,8 @@
 return {
     "mfussenegger/nvim-dap",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+    },
     config = function()
         local dap = require("dap")
         vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})

73e90a82901910101fda71bdbdcd3878eb3533d1
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-09-30 19:10:55 -0700

    style(nvim-treesitter): restructure the plugin layout

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -52,26 +52,50 @@ return {
         textobjects = {
             move = {
                 enable = true,
-                goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-                goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-                goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-                goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+                goto_next_start = {
+                    ["]f"] = "@function.outer",
+                    ["]c"] = "@class.outer"
+                },
+                goto_next_end = {
+                    ["]F"] = "@function.outer",
+                    ["]C"] = "@class.outer"
+                },
+                goto_previous_start = {
+                    ["[f"] = "@function.outer",
+                    ["[c"] = "@class.outer"
+                },
+                goto_previous_end = {
+                    ["[F"] = "@function.outer",
+                    ["[C"] = "@class.outer"
+                },
             },
             select = {
                 enable = true,
                 lookahead = true,
                 include_surrounding_whitespace = true,
                 keymaps = {
+                    ["aa"] = "@parameter.outer",
+
+                    ["al"] = "@loop.outer",
+                    ["il"] = "@loop.inner",
+
                     ["af"] = "@function.outer",
                     ["if"] = "@function.inner",
+
                     ["ac"] = "@class.outer",
                     ["ic"] = "@class.inner",
                 },
             },
             swap = {
                 enable = true,
-                swap_next = { ["<leader>a"] = "@parameter.inner" },
-                swap_previous = { ["<leader>A"] = "@parameter.inner" },
+                swap_next = {
+                    ["<leader>na"] = "@parameter.inner",
+                    ["<leader>nf"] = "@function.outer",
+                },
+                swap_previous = {
+                    ["<leader>pa"] = "@parameter.inner",
+                    ["<leader>pf"] = "@function.outer",
+                },
             },
         },
     },

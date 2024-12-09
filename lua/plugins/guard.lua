497177da772479ba5f648f2f4c3570dc1810de48
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-12-09 15:14:46 -0800

    fix(guard): remove config

--- lua/plugins/guard.lua
+++ lua/plugins/guard.lua
@@ -2,11 +2,4 @@ return {
     "nvimdev/guard.nvim",
     ft = { "lua", "cpp", "markdown" },
     dependencies = { "nvimdev/guard-collection" },
-    config = function()
-        vim.g.guard_config = {
-            fmt_on_save = true,
-            lsp_as_default_formatter = false,
-            save_on_fmt = true,
-        }
-    end,
 }

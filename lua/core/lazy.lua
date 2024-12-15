8bae502a98b152123e55016ea57f319c9916b4a4
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-12-15 07:05:19 -0800

    chore(lazy): cleanup

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -1,28 +1,19 @@
 -- Automatically install Lazy on startup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Automatically source all configs in plugins directory
 require("lazy").setup("plugins", {
-	change_detection = {
-		notify = false,
-	},
-	ui = {
-        -- border = "single",
-        border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-		-- size = {
-		-- 	width = 0.8,
-		-- 	height = 0.8,
-		-- },
-	},
+    change_detection = { notify = false, },
+    ui = { border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" } },
 })

532010c56ad9dc19855e5877d27cf3507415a4fc
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-12-15 07:50:41 -0800

    fix(nvim-highlight-colors): restore config

--- lua/plugins/nvim-highlight-colors.lua
+++ lua/plugins/nvim-highlight-colors.lua
@@ -1,9 +1,25 @@
 return {
     "brenoprata10/nvim-highlight-colors",
     enabled = true,
-    opts = {
-        render = 'virtual',
-        virtual_symbol = '',
-        virtual_symbol_position = 'eol',
-    }
+    config = function()
+        require("nvim-highlight-colors").setup {
+            render = 'virtual',
+            virtual_symbol = '',
+            virtual_symbol_suffix = '',
+            virtual_symbol_position = 'eol',
+
+            enable_hex = true,
+            enable_short_hex = true,
+            enable_rgb = true,
+            enable_hsl = true,
+            enable_var_usage = true,
+            enable_named_colors = false,
+            enable_tailwind = false,
+
+            custom_colors = {
+                { label = '%-%-theme%-primary%-color', color = '#0f1219' },
+                { label = '%-%-theme%-secondary%-color', color = '#5a5d64' },
+            },
+        }
+    end
 }

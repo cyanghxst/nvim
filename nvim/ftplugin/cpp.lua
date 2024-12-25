a30a35023492096f7844f1c167ee8492094124e1
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-12-25 00:46:41 -0800

    fix(cpp): fix preprocessor highlighting

--- nvim/ftplugin/cpp.lua
+++ nvim/ftplugin/cpp.lua
@@ -1,3 +1,7 @@
 local ft = require('guard.filetype')
 
+-- Setup clang-format
 ft("cpp"):fmt("clang-format")
+
+-- Fix syntax highlighting for preprocessor directives
+vim.api.nvim_set_hl(0, '@lsp.type.comment.cpp', {})

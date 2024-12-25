85099c4562fa8e87e3ec9c4c07ca4db7ae126492
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-12-25 05:43:23 -0800

    feat(plugins): add comment.lua

---
+++ nvim/lua/plugins/comment.lua
@@ -0,0 +1,37 @@
+return {
+    "numTostr/Comment.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+        padding = true,
+        sticky = true,
+        ignore = nil,
+
+        ---LHS of toggle mappings in NORMAL mode
+        toggler = {
+            line = 'gcc',
+            block = 'gbc',
+        },
+
+        ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+        opleader = {
+            line = 'gc',
+            block = 'gb',
+        },
+
+        ---LHS of extra mappings
+        extra = {
+            above = 'gcO',
+            below = 'gco',
+            eol = 'gcA',
+        },
+
+        ---Enable keybindings
+        mappings = {
+            basic = true,
+            extra = true,
+        },
+
+        pre_hook = nil,
+        post_hook = nil,
+    }
+}

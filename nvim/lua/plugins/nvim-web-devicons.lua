3ee5dbbea9735d1e6abc5e6092856d3319130b1e
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2025-02-02 03:31:56 -0800

    chore(devicons): add more icons

--- nvim/lua/plugins/nvim-web-devicons.lua
+++ nvim/lua/plugins/nvim-web-devicons.lua
@@ -15,6 +15,43 @@ return {
                 cterm_color = "214",
                 name = "zip",
             },
+            makefile = {
+                icon = "󰬔",
+                color = "#9ece6a",
+                -- color = "#41a6b5",
+                cterm_color = "156",
+                name = "Makefile",
+            },
+            header = {
+                icon = "",
+                color = "#bb9af7",
+                cterm_color = "183",
+                name = "H",
+            },
+            cpp = {
+                icon = "",
+                color = "#7dcfff",
+                cterm_color = "14",
+                name = "Cpp",
+            },
+            docx = {
+                icon = "󰈬",
+                color = "#7aa2f7",
+                cterm_color = "26",
+                name = "Docx",
+            },
+            py = {
+                icon = "",
+                color = "#7aa2f7",
+                cterm_color = "26",
+                name = "Py",
+            },
+            sh = {
+                icon = "",
+                color = "#6d8086",
+                cterm_color = "66",
+                name = "Sh",
+            },
             default_icon = {
                 icon = "󰈔",
                 color = "#6d8086",

aa9dff4db44df5dd5b5a6238a60be113bef936ba
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2025-10-13 13:25:55 -0700

    chore: use plugins in require spec and remove unused lsp border option

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,9 +5,7 @@ if vim.fn.has("termguicolors") == 1 then
     vim.o.termguicolors = true
 end
 
-require("core.lazy")
+require("core.plugins")
 require("core.options")
 require("core.autocmds")
 require("core.keymaps")
-
-require('lspconfig.ui.windows').default_options.border = 'single'

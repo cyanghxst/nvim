973d35866b275050f282f5984cb5bc7ec9a806eb
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2025-10-13 18:32:03 -0700

    refactor(lsp): consolidate lsp server config to mason.lua and move them into a handlers table to resolve deprecation warning

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -22,15 +22,93 @@ return {
             },
         })
 
+        local capabilities = require("blink.cmp").get_lsp_capabilities()
+
         require("mason-lspconfig").setup({
-            ensure_installed = { "html", "lua_ls", "pyright", "jdtls", "clangd" },
-            automatic_installation = true,
+            ensure_installed = { "html", "lua_ls", "pyright", "jdtls", "clangd", "marksman" },
+            handlers = {
+                function(server_name) -- default handler
+                    require("lspconfig")[server_name].setup({
+                        capabilities = capabilities,
+                    })
+                end,
+
+                lua_ls = function()
+                    require("lspconfig").lua_ls.setup({
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                diagnostics = {
+                                    globals = { "vim", "require", "hs" },
+                                },
+                                workspace = { checkThirdParty = false },
+                                completion = { callSnippet = "Replace" },
+                            },
+                        },
+                    })
+                end,
+
+                clangd = function()
+                    require("lspconfig").clangd.setup({
+                        cmd = { "clangd", "--fallback-style=webkit" },
+                        filetypes = { "cpp", "c", "objc", "objcpp" },
+                        capabilities = capabilities,
+                    })
+                end,
+
+                ts_ls = function()
+                    require("lspconfig").ts_ls.setup({
+                        capabilities = capabilities,
+                        cmd = { "typescript-language-server", "--stdio" },
+                        filetypes = {
+                            "typescript",
+                            "typescriptreact",
+                            "typescript.tsx",
+                            "javascript",
+                            "javascriptreact",
+                            "javascript.jsx",
+                        },
+                        on_attach = function(client, _)
+                            client.server_capabilities.documentFormattingProvider = false
+                        end,
+                    })
+                end,
+
+                bashls = function()
+                    require("lspconfig").bashls.setup({
+                        capabilities = capabilities,
+                        filetypes = { "sh", "bash", "zsh" },
+                        cmd = { "bash-language-server", "start" },
+                    })
+                end,
+            },
         })
+
+        -- Diagnostic configuration
+        vim.diagnostic.config({
+            virtual_text = { prefix = "*" },
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.HINT] = "*",
+                    [vim.diagnostic.severity.INFO] = "*",
+                    [vim.diagnostic.severity.WARN] = "*",
+                    [vim.diagnostic.severity.ERROR] = "*",
+                },
+            },
+        })
+
+        -- Hover and signature handlers with rounded borders
+        vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+
+        vim.lsp.handlers["textDocument/signatureHelp"] =
+            vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 
         require("mason-tool-installer").setup({
             ensure_installed = {
                 "java-debug-adapter",
                 "java-test",
+                "prettierd",
+                "stylua",
             },
         })
     end,

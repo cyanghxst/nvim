80bff79c56e180707045bff09c4a57d8957c05aa
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2025-10-13 18:40:40 -0700

    chore(telescope): enable preview for live_grep pickers

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -32,7 +32,7 @@ return {
 
         pickers = {
             find_files = { theme = "dropdown" },
-            live_grep = { theme = "dropdown" },
+            live_grep = { previewer = true },
             help_tags = { theme = "dropdown" },
             highlights = { theme = "dropdown" },
             buffers = { theme = "dropdown" },

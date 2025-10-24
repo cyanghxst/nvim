d6ae035dd2dd47d25d56a692c86b8b7a229533c2
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2025-10-24 08:52:00 -0700

    feat(plugins): add none-ls.lua

---
+++ nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,93 @@
+return {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "jay-babu/mason-null-ls.nvim",
+    },
+
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        require("mason").setup()
+        require("mason-null-ls").setup({
+            ensure_installed = {
+                "stylua", -- lua
+                "prettierd", -- js/ts/css/html/json/md
+                "google_java_format", -- java formatter
+
+                -- diagnostics & lint
+                "checkstyle", -- java style checker
+
+                -- eslint_d here
+            },
+
+            automatic_installation = true, -- formatting and diagnostics via none-ls
+            handlers = {}, -- let mason-null-ls register sources automatically
+        })
+
+        local null_ls = require("null-ls")
+        local format_group = vim.api.nvim_create_augroup("NoneLsFormatOnSave", {})
+
+        null_ls.setup({
+            sources = {
+
+                --------------
+                -- FORMATTING
+                --------------
+
+                -- lua
+                null_ls.builtins.formatting.stylua,
+
+                -- js/ts/css/html/json/md
+                null_ls.builtins.formatting.prettierd or null_ls.builtins.formatting.prettier, -- prefer prettierd. if it doesn't exist, fall back to prettier
+
+                -- java
+                null_ls.builtins.formatting.google_java_format,
+
+                -- java but AOSP style
+                -- null_ls.builtins.formatting.google_java_format.with({
+                --     extra_args = { "--aosp" },
+                -- })
+
+                ---------------
+                -- DIAGNOSTICS
+                ---------------
+
+                -- look for java checkstyle in the root directory
+                null_ls.builtins.diagnostics.checkstyle.with({
+                    extra_args = {
+                        "-c",
+                        vim.fn.getcwd() .. "/checkstyle.xml",
+                    },
+                }),
+
+                -- optional: eslint_d for js/ts diagnostics (fast asf)
+                -- null_ls.builtins.diagnostics.eslint_d,
+                -- null_ls.builtins.code_actions.eslint_d,
+            },
+
+            on_attach = function(client, bufnr)
+                -- format on save for any buffer
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({
+                        group = format_group,
+                        buffer = bufnr,
+                    })
+
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = format_group,
+                        buffer = bufnr,
+                        callback = function()
+                            -- use only null-ls for formatting
+                            vim.lsp.buf.format({
+                                bufnr = bufnr,
+                                filter = function(c)
+                                    return c.name == "null-ls"
+                                end,
+                            })
+                        end,
+                    })
+                end
+            end,
+        })
+    end,
+}

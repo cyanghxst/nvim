71c0d638af307a9e2d7af9a6bfe9135d1f7d98d8
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2025-10-25 06:57:41 -0700

    feat(none-ls): refine none-ls setup and exclude java from null-ls formatting, add checkstyle

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -15,67 +15,52 @@ return {
                 "prettierd", -- js/ts/css/html/json/md
                 -- "eslint_d", -- js/ts
             },
+            automatic_installation = true,
+            handlers = {
+                -- default handler: do normal setup for everything else
+                function() end,
 
-            automatic_installation = true, -- formatting and diagnostics via none-ls
-            handlers = {}, -- let mason-null-ls register sources automatically
+                -- custom handler for checkstyle: do nothing here
+                checkstyle = function() end,
+            },
         })
 
         local null_ls = require("null-ls")
-        local format_group = vim.api.nvim_create_augroup("NoneLsFormatOnSave", {})
+        local format_group = vim.api.nvim_create_augroup("NoneLsFormatOnSave", { clear = false })
 
         null_ls.setup({
             sources = {
-
-                --------------
-                -- FORMATTING
-                --------------
-
-                -- lua
+                ----------------
+                -- Formatters --
+                ----------------
                 null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettierd or null_ls.builtins.formatting.prettier,
 
-                -- js/ts/css/html/json/md
-                null_ls.builtins.formatting.prettierd or null_ls.builtins.formatting.prettier, -- prefer prettierd. if it doesn't exist, fall back to prettier
-
-                -- java
-                null_ls.builtins.formatting.google_java_format,
-
-                -- java but AOSP style
-                -- null_ls.builtins.formatting.google_java_format.with({
-                --     extra_args = { "--aosp" },
-                -- })
-
-                ---------------
-                -- DIAGNOSTICS
-                ---------------
-
-                -- look for java checkstyle in the root directory
+                -----------------
+                -- Diagnostics --
+                -----------------
                 null_ls.builtins.diagnostics.checkstyle.with({
-                    extra_args = {
-                        "-c",
-                        vim.fn.getcwd() .. "/checkstyle.xml",
-                    },
+                    extra_args = { "-c", vim.env.HOME .. "/.config/java/checkstyle.xml" },
                 }),
 
-                -- optional: eslint_d for js/ts diagnostics (fast asf)
+                -- js/ts diagnostics:
                 -- null_ls.builtins.diagnostics.eslint_d,
                 -- null_ls.builtins.code_actions.eslint_d,
             },
 
             on_attach = function(client, bufnr)
-                -- format on save for any buffer
-                if client.supports_method("textDocument/formatting") then
-                    vim.api.nvim_clear_autocmds({
-                        group = format_group,
-                        buffer = bufnr,
-                    })
+                local ft = vim.bo[bufnr].filetype
 
+                -- use null-ls to format everything except java
+                if client.name == "null-ls" and client.supports_method("textDocument/formatting") and ft ~= "java" then
+                    vim.api.nvim_clear_autocmds({ group = format_group, buffer = bufnr })
                     vim.api.nvim_create_autocmd("BufWritePre", {
                         group = format_group,
                         buffer = bufnr,
                         callback = function()
-                            -- use only null-ls for formatting
                             vim.lsp.buf.format({
                                 bufnr = bufnr,
+                                async = false,
                                 filter = function(c)
                                     return c.name == "null-ls"
                                 end,

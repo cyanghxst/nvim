6e23a2c18c0888f9f657b5e8d3c139e810f0f548
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2026-03-07 18:29:50 -0800

    fix(autocmds): update autoclose tree

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -18,7 +18,7 @@ vim.api.nvim_create_autocmd("Filetype", {
 
 -- Autoclose tree
 vim.api.nvim_create_autocmd("BufEnter", {
-    command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
+    command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit! | endif",
     nested = true,
 })
 

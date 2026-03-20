e0a996f6810e6fbbcf8a0ffbcce78cb1d2589c97
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2026-03-20 16:43:01 -0700

    feat(plugins): add which-key and trouble.nvim
    - which-key: group prefixes for leader key discoverability
    - trouble: diagnostics list with <leader>xx and <leader>xb

---
+++ nvim/lua/plugins/which-key.lua
@@ -0,0 +1,19 @@
+return {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {},
+    config = function(_, opts)
+        local wk = require("which-key")
+        wk.setup(opts)
+
+        wk.add({
+            { "<leader>f", group = "Find" },
+            { "<leader>g", group = "LSP" },
+            { "<leader>w", group = "Window/Buffer" },
+            { "<leader>n", group = "Swap Next" },
+            { "<leader>p", group = "Swap Previous" },
+            { "<leader>rr", desc = "Rename symbol" },
+            { "<leader>s", desc = "Toggle NvimTree" },
+        })
+    end,
+}

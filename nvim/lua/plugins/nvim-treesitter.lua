48a9c870773e8195204ddffe8317bf8a4a386ff8
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2026-04-06 21:18:51 -0700

    refactor(treesitter): simplify treesitter config

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,106 +1,28 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = "nvim-treesitter/nvim-treesitter-textobjects",
-    opts = {
-        auto_install = true,
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false,
-            disable = { "tmux" },
-        },
-
-        indent = {
-            enable = true,
-            disable = { "python", "java", "html" },
-        },
-
-        ensure_installed = {
-            "query",
-            "html",
-            "javascript",
-            "java",
-            "css",
-            "vim",
-            "vimdoc",
-            "lua",
-            "luadoc",
-            "bash",
-            "json",
-            "toml",
-            "yaml",
-            "sql",
-            "markdown",
-            "markdown_inline",
-            "python",
-            "regex",
-            "c",
-            "make",
-            "doxygen",
-            "cpp",
-        },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<C-space>",
-                node_incremental = "<C-space>",
-                scope_incremental = false,
-                node_decremental = "<bs>",
-            },
-        },
-        textobjects = {
-            move = {
-                enable = true,
-                goto_next_start = {
-                    ["]f"] = "@function.outer",
-                    ["]c"] = "@class.outer",
-                },
-                goto_next_end = {
-                    ["]F"] = "@function.outer",
-                    ["]C"] = "@class.outer",
-                },
-                goto_previous_start = {
-                    ["[f"] = "@function.outer",
-                    ["[c"] = "@class.outer",
-                },
-                goto_previous_end = {
-                    ["[F"] = "@function.outer",
-                    ["[C"] = "@class.outer",
-                },
-            },
-            select = {
-                enable = true,
-                lookahead = true,
-                include_surrounding_whitespace = true,
-                keymaps = {
-                    ["aa"] = "@parameter.outer",
-
-                    ["al"] = "@loop.outer",
-                    ["il"] = "@loop.inner",
-
-                    ["af"] = "@function.outer",
-                    ["if"] = "@function.inner",
-
-                    ["ac"] = "@class.outer",
-                    ["ic"] = "@class.inner",
-                },
-            },
-            swap = {
-                enable = true,
-                swap_next = {
-                    ["<leader>na"] = "@parameter.inner",
-                    ["<leader>nf"] = "@function.outer",
-                },
-                swap_previous = {
-                    ["<leader>pa"] = "@parameter.inner",
-                    ["<leader>pf"] = "@function.outer",
-                },
-            },
-        },
+    branch = "main",
+    dependencies = {
+        { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
     },
-    config = function(_, opts)
+    config = function()
+        require("nvim-treesitter").setup {}
         vim.filetype.add({ extension = { mdx = "markdown" } })
-        require("nvim-treesitter.configs").setup(opts)
+
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function(args)
+                pcall(vim.treesitter.start, args.buf)
+                vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
+
+        local parsers = {
+            "query", "html", "javascript", "java", "css", "vim", "vimdoc",
+            "lua", "luadoc", "bash", "json", "toml", "yaml", "sql",
+            "markdown", "markdown_inline", "python", "regex", "c",
+            "make", "doxygen", "cpp",
+        }
+        require("nvim-treesitter").install(parsers)
     end,
 }

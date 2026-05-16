1ea6945aad1f61dd753f2c6c3c43d18736268605
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2026-05-16 06:37:11 -0700

    chore(plugins): remove trouble.nvim

--- nvim/lua/plugins/trouble.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "folke/trouble.nvim",
-    cmd = "Trouble",
-    keys = {
-        { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
-        { "<leader>xb", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
-    },
-    opts = {},
-}
